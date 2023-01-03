3963ec4c6b4b4c59d7dca5b01177d4b13338b84a
Mojiayi <woshimojiayi@gmail.com>
2023-01-03 22:47:02 +0800

    修改接口访问记录打印格式

--- middleware/RequestMetadataLogHandler.go
+++ middleware/RequestMetadataLogHandler.go
@@ -1,12 +1,28 @@
 package middleware
 
 import (
+	"encoding/json"
 	"mojiayi-go-journey/setting"
 	"time"
+	"unsafe"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
 )
+
+type RequestMetadata struct {
+	Cost   int64  `json:"cost"`
+	IP     string `json:"ip"`
+	Method string `json:"method"`
+	URI    string `json:"uri"`
+}
+
+func (c *RequestMetadata) String() string {
+	byteArray, err := json.Marshal(c)
+	if err == nil {
+		return *(*string)(unsafe.Pointer(&byteArray))
+	}
+	return ""
+}
 
 func RecordCostTime() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -14,12 +30,12 @@ func RecordCostTime() gin.HandlerFunc {
 
 		ctx.Next()
 
-		setting.MetadataLogger.WithFields(logrus.Fields{
-			"cost":   time.Since(startTime).Milliseconds(),
-			"ip":     ctx.ClientIP(),
-			"method": ctx.Request.Method,
-			"uri":    ctx.Request.RequestURI,
-			"usage":  "metadata",
-		}).Info("requestMetadata")
+		var requestMetadata = new(RequestMetadata)
+		requestMetadata.Cost = time.Since(startTime).Milliseconds()
+		requestMetadata.IP = ctx.ClientIP()
+		requestMetadata.Method = ctx.Request.Method
+		requestMetadata.URI = ctx.Request.RequestURI
+
+		setting.MetadataLogger.Info(requestMetadata)
 	}
 }
